d313a065b95a4ece0d76be798569c956b8cb342e
Lubos <tridosm@gmail.com>
2023-11-12 10:15:30 +0100

    Process everything in one pass

--- meshall.py
+++ meshall.py
@@ -1,13 +1,13 @@
 import glob
 import re
 import os
-import shutil
 import vtk
 
 
 def nii_2_mesh(filename_nii, filename_obj, label):
 
     # read the file
+    print(filename_nii)
     reader = vtk.vtkNIFTIImageReader()
     reader.SetFileName(filename_nii)
     reader.Update()
@@ -18,7 +18,7 @@ def nii_2_mesh(filename_nii, filename_obj, label):
     surf.SetValue(0, label) # use surf.GenerateValues function if more than one contour is available in the file
     surf.Update()
     
-    #smoothing the mesh
+    # smoothing the mesh
     smoother= vtk.vtkWindowedSincPolyDataFilter()
     smoother.SetInputConnection(surf.GetOutputPort())
     smoother.SetNumberOfIterations(100) 
@@ -36,35 +36,44 @@ def nii_2_mesh(filename_nii, filename_obj, label):
 
 if __name__ == '__main__':
 
-    # create 3D model per object
-    output = "temp/"
-    if os.path.exists(output):
-        shutil.rmtree(output)
-    os.makedirs(output)
-    for file in glob.glob("segmentations/*.gz"):
-        nii_2_mesh(file, output + os.path.basename(file) + ".obj", 1)
-
-    # merge 3D models into one and colorize them
+    # prepare variables
     color = 0
     offset = 0
+    temp = "temp.obj"
+
+    # write header of the output file
     output = open('output.obj', 'w')
     output.write("mtllib material.mtl\n")
     output.write("usemtl default\n")
-    for file in glob.glob("temp/*.obj"):
+
+    # run the loop
+    for file in glob.glob("segmentations/*.gz"):
+
+        # generate mesh for a single object
         count = 0
-        print(file)
-        output.write("o " + os.path.basename(file) + "\n")
-        with open(file) as f:
-            for line in re.split("\n", f.read()):
-                data = re.split("\s+", line)
-                if data[0] == 'v':
-                    output.write(line + "\n")
-                    output.write("vt " + str(color) + " 0\n")
-                    count += 1
-                if data[0] == 'f':
-                    a = str(int(data[1]) + offset)
-                    b = str(int(data[2]) + offset)
-                    c = str(int(data[3]) + offset)
-                    output.write("f " + a + "/" + a + " " + b + "/" + b + " " + c + "/" + c + "\n")
-        offset += count
-        color += 1.0 / 64.0 #Color range is 0..1 but it is better to have contrast colors than unique colors
+        nii_2_mesh(file, temp, 1)
+
+        # append the mesh of the single object into the output
+        if os.path.isfile(temp):
+            output.write("o " + os.path.basename(file) + "\n")
+            with open(temp) as f:
+                for line in re.split("\n", f.read()):
+                    data = re.split("\s+", line)
+
+                    # write vertex coordinates
+                    if data[0] == 'v':
+                        output.write(line + "\n")
+                        output.write("vt " + str(color) + " 0\n")
+                        count += 1
+
+                    # write triangle indices
+                    if data[0] == 'f':
+                        a = str(int(data[1]) + offset)
+                        b = str(int(data[2]) + offset)
+                        c = str(int(data[3]) + offset)
+                        output.write("f " + a + "/" + a + " " + b + "/" + b + " " + c + "/" + c + "\n")
+
+            # update indices offset, object color and remove temp file
+            offset += count
+            color += 1.0 / 64.0 #Color range is 0..1 but it is better to have contrast colors than unique colors
+            os.remove(temp)
