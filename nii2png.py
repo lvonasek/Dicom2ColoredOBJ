0dd5b3ef7e2ae689bcdb7609543bac7fce6f287f
Lubos <tridosm@gmail.com>
2023-11-15 20:27:04 +0100

    Support multiple nii shapes

--- nii2png.py
+++ nii2png.py
@@ -38,30 +38,8 @@ def main(argv):
     # set fn as your 4d nifti file
     image_array = nibabel.load(inputfile).get_fdata()
 
-    # if 4D image inputted
-    if len(image_array.shape) == 4:
-        # set 4d array dimension values
-        nx, ny, nz, nw = image_array.shape
-
-        # set destination folder
-        if not os.path.exists(outputfile):
-            os.makedirs(outputfile)
-
-        total_volumes = image_array.shape[3]
-        total_slices = image_array.shape[2]
-
-        # iterate through volumes
-        for current_volume in range(0, total_volumes):
-            # iterate through slices
-            for current_slice in range(0, total_slices):
-                data = image_array[:, :, current_slice, current_volume]
-                        
-                #alternate slices and save as png
-                image_name = inputfile[:-4] + "_t" + "{:0>3}".format(str(current_volume+1)) + "_z" + "{:0>3}".format(str(current_slice+1))+ ".png"
-                imageio.imwrite(image_name, Image.fromarray(data))
-
-    # else if 3D image inputted
-    elif len(image_array.shape) == 3:
+    # if 3D image inputted
+    if len(image_array.shape) == 3:
         # set 4d array dimension values
         nx, ny, nz = image_array.shape
 
@@ -69,18 +47,30 @@ def main(argv):
         if not os.path.exists(outputfile):
             os.makedirs(outputfile)
 
-        total_slices = image_array.shape[0]
+        if image_array.shape[1] == image_array.shape[2]:
+            total_slices = image_array.shape[0]
+        elif image_array.shape[0] == image_array.shape[2]:
+            total_slices = image_array.shape[1]
+        elif image_array.shape[0] == image_array.shape[1]:
+            total_slices = image_array.shape[2]
+        else:
+            print('Unsupported resolution')
 
         # iterate through slices)
         for current_slice in range(0, total_slices):
             # alternate slices
-            data = image_array[current_slice, :, :]
+            if image_array.shape[1] == image_array.shape[2]:
+                data = image_array[current_slice, :, :]
+            elif image_array.shape[0] == image_array.shape[2]:
+                data = image_array[:, current_slice, :]
+            elif image_array.shape[0] == image_array.shape[1]:
+                data = image_array[:, :, current_slice]
 
             #alternate slices and save as png
             image_name = inputfile[:-4] + "_z" + "{:0>3}".format(str(current_slice+1))+ ".png"
             imageio.imwrite(image_name, Image.fromarray(data).convert("L"))
     else:
-        print('Not a 3D or 4D Image. Please try again.')
+        print('Not a 3D Image. Please try again.')
 
 # call the function to start the program
 if __name__ == "__main__":
